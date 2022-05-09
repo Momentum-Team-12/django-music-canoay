4e931f9548ca171f31f7769a0aef364d377857f4
Ambar Cano <ambarcano@gmail.com>
2022-05-09 15:26:00 -0400

    minor fix, pending img resolution

--- albums/models.py
+++ albums/models.py
@@ -1,10 +1,10 @@
-from datetime import datetime
+import datetime
 from django.db import models
 
 # Create your models here.
 
 class Album(models.Model):
     album_title = models.CharField(max_length=455)
-    image = models.ImageField(upload_to='images/', null=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
     artist = models.CharField(max_length=455)
     created_at = models.DateTimeField(auto_now_add=True)
